a4b2e37698ef51230ff381153244d9d03ab3cc15
hossamgado <hossammahmoudgado@gmail.com>
2025-09-07 17:58:55 +0300

    lunch splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
+import 'package:ecommerce_app/config/routes_manager/routes.dart';
+import 'package:ecommerce_app/config/routes_manager/routes_generator.dart';
 import 'package:ecommerce_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(EcommerceApp());
 }
+
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: "Poppins",
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        theme: ThemeData(fontFamily: "Poppins"),
+        debugShowCheckedModeBanner: false,
+        home: child,
+        onGenerateRoute: RouteGenerator.getRoute,
+        initialRoute: Routes.signInRoute,
       ),
-      debugShowCheckedModeBanner: false,
-        home: Scaffold()
     );
   }
 }
